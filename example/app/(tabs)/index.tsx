f08021f02b3754437c291b8ebb69d809f9bba134
Hashith Karunarathne <hashithkarunarathne@live.com>
2025-07-31 22:22:15 +0800

    chore(example): update example

--- example/app/(tabs)/index.tsx
+++ example/app/(tabs)/index.tsx
@@ -1,5 +1,7 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Platform, StyleSheet, Alert } from 'react-native';
+import { useEffect } from 'react';
+import { useOrientation } from '@el173/react-native-use-orientation';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
@@ -7,6 +9,12 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
+  const orientation = useOrientation();
+
+  useEffect(() => {
+    Alert.alert('Orientation Changed', `New orientation: ${orientation}`);
+  }, [orientation]);
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
